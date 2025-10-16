7b4f287a8bf5739ad7a7cef1749fc6c0f0f1d4c1
navidras <navidras@gmail.com>
2025-10-16 04:35:43 +0330

    RedisActions notincludes

--- app/utils/RedisActions.js
+++ app/utils/RedisActions.js
@@ -1,36 +1,34 @@
 export class RedisActions {
-
-    static async getUserDetail(client, key) {
-        let strRes = await client.get(key);
-        let objRes = JSON.parse(strRes);
-        return objRes;
+  static async getUserDetail(client, key) {
+    if (!key.includes("backup")) {
+      let strRes = await client.get(key);
+      let objRes = JSON.parse(strRes);
+      return objRes;
     }
+  }
 
-    static async checkIfUserSocketExists(server, client, key) {
-        let strRes = await client.get(key);
-        if (strRes) {
-            let objRes = JSON.parse(strRes);
-            // objRes.socketId;
-            let foundSocket = server.sockets.sockets.get(objRes.socketId);
-            if (foundSocket) {
-               await foundSocket.disconnect(true);
-            }
-        }
-
-    }
-
-    static async disconnectCurrentUser(socket, client, key) {
-        let strRes = await client.get(key);
-        if (strRes) {
-            let objRes = JSON.parse(strRes);
-            // objRes.socketId;
-               await socket.disconnect(true);
-            
-        }
-    }
-
-    static wait(milliseconds) {
-        return new Promise(resolve => setTimeout(resolve, milliseconds));
+  static async checkIfUserSocketExists(server, client, key) {
+    let strRes = await client.get(key);
+    if (strRes) {
+      let objRes = JSON.parse(strRes);
+      // objRes.socketId;
+      let foundSocket = server.sockets.sockets.get(objRes.socketId);
+      if (foundSocket) {
+        await foundSocket.disconnect(true);
       }
+    }
+  }
 
+  static async disconnectCurrentUser(socket, client, key) {
+    let strRes = await client.get(key);
+    if (strRes) {
+      let objRes = JSON.parse(strRes);
+      // objRes.socketId;
+      await socket.disconnect(true);
+    }
+  }
+
+  static wait(milliseconds) {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds));
+  }
 }
